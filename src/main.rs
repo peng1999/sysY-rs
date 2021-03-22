c32986e6360dfd442e67a413df998ec480798d16
Peng Guanwen <pg999w@outlook.com>
2021-03-22 08:40:15 +0800

    Remove pest

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 #![feature(io_read_to_string)]
 
-#[macro_use] extern crate pest_derive;
 #[macro_use] extern crate lalrpop_util;
 
 mod ast;
 mod lexer;
-mod parser;
 lalrpop_mod!(pub syntax);
 mod llvm;
 
@@ -16,11 +14,7 @@ fn main() -> anyhow::Result<()> {
     let arg1 = std::env::args().nth(1);
     if let Some("lexer") = arg1.as_deref() {
         lexer::run(source);
-    } else if let Some("pest") = arg1.as_deref() {
-        parser::run(source);
     } else if let Some("ast") = arg1.as_deref() {
-        // let ast_tree = parser::parse(source)?;
-        // println!("{:#?}", ast_tree);
         let parser = syntax::StmtParser::new();
         let ast_tree = parser.parse(&source);
         println!("{:#?}", ast_tree);
